3e09b1c1a0d04ffe69e7b57391d91b6c63c6be2a
SaurabhNandaniya <22bce203@nirmauni.ac.in>
2025-11-10 14:26:40 +0000

    datatypes learned

---
+++ js_study/js_01.js
@@ -0,0 +1,16 @@
+const accountId = 1243245
+let accountName = "Saurabh"
+var accountEmail ="saurabh@gmail.com"
+accountCity = "Ahmedabad" // this also not preferable to use in js 
+let stateOf
+let accountState =""
+// accountId = 4567 // we cant change the const value
+/*
+we simple onl use const and let var have issue with the scope like it changes all the
+variable with it 
+*/
+accountName = "hello"
+accountEmail = "@1332"
+accountCity = "loej"
+console.log(accountId);
+console.table([accountName, accountEmail, accountCity, stateOf, accountState])
---
+++ js_study/js_datatypes_02.js
@@ -0,0 +1,23 @@
+"use strict"; // use all cose as a new java versio
+//alert( 3 + 3) // we are using node js not a browser
+
+console.log( 3 + 3)
+console.log("saurabh")
+
+let age = 21
+let name = "saurabh"
+let availability = true
+let onlyUndefine;
+
+// 2 to power 53 or we can use bigint
+//string ""
+// number 
+// boolean
+// null standalone value like we have to give it not like undefined or zero
+//symbol => unique
+
+// oblect is like big thing like nxt videos
+console.log(typeof age);
+console.log(typeof availability);
+console.log(typeof null);//oblect
+console.log(typeof undefined);//undefined
