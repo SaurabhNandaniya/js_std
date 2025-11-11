5411bcabeb1417437ef097071748e42eb38617ed
SaurabhNandaniya <22bce203@nirmauni.ac.in>
2025-11-11 05:07:04 +0000

    operations done

--- js_study_01/js_conversion.js
+++ js_study_01/js_conversion.js
@@ -1,11 +1,11 @@
 let age = "123" //null undefined 23abc true/false string=>NaN etc..
 
-console.log(typeof age);
+// console.log(typeof age);
 let valueInNumber = Number(age)
 
-console.log(typeof valueInNumber);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,13 +15,50 @@ console.log(valueInNumber);
 let llog = "saurabh" // "" => false "saurabh" => true 1=>true 0=>false
 
 let llogBolean = Boolean(llog)
-console.log(llogBolean);
+// console.log(llogBolean);
 
 let ageInNumber = 20
 
 let ageInString = String(ageInNumber)
-console.log(ageInString);
-console.log(typeof ageInString);
+// console.log(ageInString);
+// console.log(typeof ageInString);
 
 
-//        operations        
+
+
+
+//        operations       
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
