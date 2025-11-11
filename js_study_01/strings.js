5f28abf399a79b3be6ef01f1902e0734c5798e7f
SaurabhNandaniya <22bce203@nirmauni.ac.in>
2025-11-11 12:18:19 +0000

    strings basic ops covered

---
+++ js_study_01/strings.js
@@ -0,0 +1,34 @@
+const name = "Saurabh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`my name is ${name} and i have this much repo ${repoCount}`);
+
+const gameName = new String("saurabh-dot-com")
+
+// console.log(gameName[0]);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(2, -6)
+console.log(anotherString);
+
+const newStringOne = "   saurabh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://saurabh.com/saurabh%20ahir"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('.com'))
+
+// console.log(gameName.split('-'));
