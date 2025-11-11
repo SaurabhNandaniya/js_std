45e19fb5f40c7868f4d889d2f25d451d82dbc89d
SaurabhNandaniya <22bce203@nirmauni.ac.in>
2025-11-11 04:52:00 +0000

    conversions

---
+++ js_study_01/js_conversion.js
@@ -0,0 +1,27 @@
+let age = "123" //null undefined 23abc true/false string=>NaN etc..
+
+console.log(typeof age);
+let valueInNumber = Number(age)
+
+console.log(typeof valueInNumber);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+let llog = "saurabh" // "" => false "saurabh" => true 1=>true 0=>false
+
+let llogBolean = Boolean(llog)
+console.log(llogBolean);
+
+let ageInNumber = 20
+
+let ageInString = String(ageInNumber)
+console.log(ageInString);
+console.log(typeof ageInString);
+
+
+//        operations        
