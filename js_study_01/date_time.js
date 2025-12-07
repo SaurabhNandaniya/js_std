f9882fdc0973b28da815d1068eb4a1ae44d109cc
SaurabhNandaniya <22bce203@nirmauni.ac.in>
2025-12-07 06:05:38 +0000

    date and time done

---
+++ js_study_01/date_time.js
@@ -0,0 +1,30 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);//object 
+
+// let myCreatedDate = new Date(2025, 11, 10)
+// myCreatedDate = new Date(2025, 11, 10, 12, 2, 90)
+// myCreatedDate = new Date("2025-03-23")
+myCreatedDate = new Date("01-23-2034")
+// console.log(myCreatedDate.toLocaleString());
+
+let timeStamp = Date.now()
+// console.log(timeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Date.now());// this in mili second
+// console.log(Math.floor(Date.now()/1000));// this in second
+
+let newDate = new Date()
+console.log(newDate.getDate());
+console.log(newDate.getMonth() + 1);//months start with the 0
+// we can print sepearte dat month year and time with 
+// string interpolation with below
+// console.log(`the date is ${newDate.getDate()} and the month is ${newDate.getMonth()+1}.`);
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+    minute : "2-digit"//control space to get the other functions
+}))
--- js_study_01/js_01.js
+++ js_study_01/js_01.js
@@ -1,9 +1,9 @@
 const accountId = 1243245
-let accountName = "Saurabh"
+let accountName = "Saurabh"//this should ne re declared
 var accountEmail ="saurabh@gmail.com"
 accountCity = "Ahmedabad" // this also not preferable to use in js 
 let stateOf
-let accountState =""
+let accountState =""//let cant be re declared and 
 // accountId = 4567 // we cant change the const value
 /*
 we simple onl use const and let var have issue with the scope like it changes all the
