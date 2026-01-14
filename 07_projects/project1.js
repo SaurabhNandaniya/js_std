f48bd4411fdaba84e5d40d02a8c125ccd912b389
SaurabhNandaniya <22bce203@nirmauni.ac.in>
2026-01-14 06:57:53 +0000

    written by me

--- 07_projects/project1.js
+++ 07_projects/project1.js
@@ -28,3 +28,13 @@ buttons.forEach( (button) => {
     
   })
 })
+
+//written by me
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body')
+
+// buttons.forEach((button) => {
+//   button.addEventListener('click', (e) => {
+//     body.style.backgroundColor = e.target.id;
+//   })
+// })
